47ba7130df1f63a50ec2cc3926fd0e7496c19523
miraclepbspbs <304088570@qq.com>
2022-10-04 03:34:33 +0800

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        jsodifjdiogmndfiojgno;adighdfnoghnifdjnsdifbnsdifbnisdulfdsabgdafihgeraiogndfa;oihdfaiogdfaiughidpuafghdfiangidfg
+      </p>
+    </section>
+  )
+}
+
+export default About
